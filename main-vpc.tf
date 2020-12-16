36c88070fd5fb4df68c9a5125e8945f533829060
chironosakanaya <75937233+chironosakanaya@users.noreply.github.com>
2020-12-16 22:41:34 +0900

    1216 commit

--- main-vpc.tf
+++ main-vpc.tf
@@ -1,3 +1,14 @@
+### main.tf
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+
+provider "aws" {
+  access_key = "var.AWS_ACCESS_KEY"
+  secret_key = "var.AWS_SECRET_KEY"
+  version = "1.4.0"
+  region = "ap-northeast-1"
+}
+
 #VPC trrraform
 resource "aws_vpc" "dev-vpc-001" {
     cidr_block = "10.1.0.0/16"
